b219daa6552addd618fcd3e7f580a21478525735
Rilamohammed <rilamohamed.3@gmail.com>
2023-09-11 13:09:06 -0700

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
-*
-*main - Entry point
-*
-*Descrription: single base 10 numbers
-*
-*Return: Always 0 (Success)
-*
-*/
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {

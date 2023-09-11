209d17d038ccab3b5ff53305bffe6348603435a1
Rilamohammed <rilamohamed.3@gmail.com>
2023-09-10 20:39:24 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
-
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-
+*main - Enty point
+*
+*Description: Displays alphabets
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 	int i;
 	for (i = 0; i < 26; i++)
 	{
-		putchar (alphabets[i]);
+		putchar(alphabets[i]);
 	}
 	putchar('\n');
 	return (0);

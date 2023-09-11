098f8685783b460062286b2d4ab66346e3c7001d
Rilamohammed <rilamohamed.3@gmail.com>
2023-09-10 20:08:32 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
-*main - Enty point
-*
-*Description: Prints alphabets
-*
-*Return: Always 0 (Success)
-*/
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
-		putchar (alphabets[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);

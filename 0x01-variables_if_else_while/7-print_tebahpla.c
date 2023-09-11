eed44e45730bd14d1f440ba6a02a67187a7bff61
Rilamohammed <rilamohamed.3@gmail.com>
2023-09-11 13:38:45 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**                                                               
+*main - Enty point                                                
+*                                                                 
+*Description: Displays alphabets                                  
+*                                                                 
+*Return: Always 0 (Success)                                       
+*/
+
+int main(void)
+{
+        char alph;
+
+        for (alph = 'z'; alph >= 'a'; alph--)
+        {
+                putchar(alph);
+        }
+        putchar('\n');
+        return (0);
+}

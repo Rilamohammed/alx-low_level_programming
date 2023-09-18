b3a740edc990f5e036e301b302b196fbd950ecf6
Rilamohammed <rilamohamed.3@gmail.com>
2023-09-18 14:10:58 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+    int *p= &n;
+    *p= 98;
+    return 0;
+}
+
+  

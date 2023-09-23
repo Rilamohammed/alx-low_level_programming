dda156f830b818a659f4fe15c6cf35e337caac08
Rilamohammed <rilamohamed.3@gmail.com>
2023-09-23 03:46:51 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabets
+ *
+*/
+
+void print_alphabet(void)
+{
+char put[] = "abcdefghijklmnopqrstuzwxyz";
+int z;
+
+for (z = 0; z < 26; z++)
+_putchar(put[z]);
+_putchar('\n');
+}

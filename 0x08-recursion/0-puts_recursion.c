54c1dfd7bf496d8f486020f8a11f2b3c3fbb9d62
Rilamohammed <rilamohamed.3@gmail.com>
2023-10-03 02:42:03 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,17 @@
+/**
+ * _puts_recursion - uses recursion to print string
+ *
+ * @s: char pointer
+ * Return: Always 0.
+*/
+
 #include "main.h"
 
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-    _putchar('\n');
+_putchar('\n');
 }
 else
 {

0e3c6dc89d95b7a073403b47eba3bf97e1422fb3
Rilamohammed <rilamohamed.3@gmail.com>
2023-10-05 03:36:19 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - array of chars
+ * @argc: the char to be filled
+ * @argv: the size of the array
+ *
+ * Return: The filled array
+ */
+
+int main(int argc, char *argv[])
+{
+int i, p = 1;
+if (argc == 3)
+{
+for (i = 1; i < argc; i++)
+{
+p = p *atoi(argv[i]);
+}
+{
+printf("%d\n", p);
+}
+}
+else
+{
+printf("Error\n");
+}
+return (1);
+}
